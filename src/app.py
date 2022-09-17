947bb29e3c4ca79a258d4b5bbc541ab91191f253
AgusFerrando <104893201+AgusFerrando@users.noreply.github.com>
2022-09-17 16:16:55 +0200

    register

--- src/app.py
+++ src/app.py
@@ -1,7 +1,10 @@
 import imp
-from flask import Flask
+from unicodedata import name
+from flask import Flask, jsonify, request
 from config import config
 from routes import users_routes
+from database.db import get_connection
+from models.entities.users import Users
 
 app = Flask(__name__)
 
@@ -18,3 +21,6 @@ if __name__ == '__main__':
     app.register_error_handler(404, page_not_found)
     app.run()
 
+
+
+# @app.get('/users/<id>')
--- src/routes/users_routes.py
+++ src/routes/users_routes.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, jsonify
+from uuid import  uuid1
+from flask import Blueprint, jsonify, request
 from models.UsersModel import UsersModel
+from database.db import get_connection
 
 main = Blueprint('users_blueprint', __name__)
 
@@ -10,3 +12,26 @@ def get_users():
         return jsonify(users)
     except Exception as ex:
         return jsonify({'message': str(ex)}), 500
+
+@main.post('/add')
+def create_user():
+    new_user= request.get_json()
+    name = new_user['name']
+    surname = new_user['surname']
+    email = new_user['email']
+    password = new_user['password']
+
+    conn = get_connection()
+    cur= conn.cursor()
+
+    userId = uuid1()
+    cur.execute('INSERT INTO users (id, name, surname, email, password) VALUES (%s,%s, %s, %s, %s) RETURNING *', (str(userId), name, surname, email, password))
+
+    new_created_user = cur.fetchone()
+    print(new_created_user)
+    conn.commit()
+
+    cur.close()
+    conn.close()
+
+    return jsonify(new_created_user)
