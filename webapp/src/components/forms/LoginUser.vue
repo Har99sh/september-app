acb3764153c11d4346789cc8dc7cfed21a6ad8eb
matias zito <matiaszito@gmail.com>
2022-10-03 08:38:21 +0200

    se le dio formato al login

--- webapp/src/components/forms/LoginUser.vue
+++ webapp/src/components/forms/LoginUser.vue
@@ -3,9 +3,8 @@
     <main>
       <div class="modal modal-signin position-static d-block py-5" tabindex="-1" role="dialog" id="modalSignin">
          <div class="modal-dialog" role="document">
-           <div class="modal-content rounded-4 shadow">
+           <div class="modal-content rounded-4 shadow" id="contenidoDelLogin">
              <div class="modal-header p-3 pb-3 border-bottom-0">
-               <!-- <h5 class="modal-title">Modal title</h5> -->
                <p class="fw-bold mb-0 fs-5">Sign in to your account</p>
                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
              </div>
@@ -15,13 +14,13 @@
                    <input type="Name" class="form-control rounded-10" id="floatingInput" placeholder="email" v-model="email">
                    <label for="floatingInput">User</label>
                  </div>
-                 <div class="form-floating mb-2">
+                 <div class="form-floating mb-3">
                      <input type="password" class="form-control rounded-10" id="floatingPassword" placeholder="Password" v-model="password">
                      <label for="floatingPassword">Password</label>
                    </div>
-                 <div class=" btn-toolbar d-flex justify-content-evenly">
-                     <button class="log  btn btn-lg rounded-3 btn-md" @click="authenticate">Login</button>
-                     <button class="logcancel btn btn-lg rounded-3 btn-md" type="submit"> Cancel</button>
+                 <div class="btn-toolbar d-flex justify-content-evenly">
+                     <button class="log  btn btn-lg rounded-3 btn-md botonDeLogin" id="botonDeLogin" @click="authenticate">Login</button>
+                     <button class="btn btn-lg rounded-3 btn-md" id="logCancel" type="submit">Cancel</button>
                  </div>
                </form>
              </div>
@@ -61,6 +60,23 @@ export default {
     }
 }
 </script>
-<style lang="">
+<style>
+
+.modal-header {
+  background-color: rgba(120, 166, 200, 1);
+}
+
+#contenidoDelLogin {
+  background-color: rgb(49, 103, 137, 1);
+}
+
+#botonDeLogin {
+  background-color: rgba(120, 166, 200, 1);
+  color: white; 
+}
+#logCancel {
+  background-color: rgba(120, 166, 200, 1);
+  color: white;
+}
     
 </style>
