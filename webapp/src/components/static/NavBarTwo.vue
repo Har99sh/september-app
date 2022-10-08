1e6fcf8abe47363b86026973a612f3c1a3ea4e09
matias zito <matiaszito@gmail.com>
2022-10-08 14:33:58 +0200

    create NavBarTwo.vue

---
+++ webapp/src/components/static/NavBarTwo.vue
@@ -0,0 +1,25 @@
+<template>
+    <nav class="navbar navbar-expand-lg bg-light">
+       <div class="container-fluid">
+          <router-link class="navbar-brand" to="/">J4W</router-link>
+          <div class="d-flex" id="navbarTogglerDemo02">
+             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                   <router-link class="nav-link active" to="/login">Check</router-link>
+                </li>
+                <li class="nav-item">
+                   <router-link class="nav-link active" to="/register-company">Logout</router-link>
+                </li>
+             </ul>
+          </div>
+       </div>
+    </nav>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+    
+</style>
