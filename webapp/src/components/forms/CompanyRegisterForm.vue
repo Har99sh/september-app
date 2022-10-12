42fab6ac8fb7a24ce50169ac9abccafdc0072733
matias zito <matiaszito@gmail.com>
2022-10-12 23:54:28 +0200

    change style companyRegisterForm

--- webapp/src/components/forms/CompanyRegisterForm.vue
+++ webapp/src/components/forms/CompanyRegisterForm.vue
@@ -1,5 +1,74 @@
 <template>
-  <div>
+
+
+<div>
+    <section class="vh-100" style="background-color: #696969;">
+              <div class="container py-5 h-100">
+                  <div class="row d-flex justify-content-center align-items-center h-100">
+                  <div class="col col-xl-10">
+                      <div class="card" style="border-radius: 1rem;">
+                      <div class="row g-0">
+                          <div class="col-md-6 col-lg-5 d-none d-md-block">
+                          <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/img1.webp"
+                              alt="login form" class="img-fluid" style="border-radius: 1rem 0 0 1rem;" />
+                          </div>
+                          <div class="col-md-6 col-lg-7 d-flex align-items-center">
+                          <div class="card-body p-4 p-lg-5 text-black">
+                              <b-form>
+                                 
+                              <div class="d-flex align-items-center mb-3 pb-1">
+                                  <i class="fas fa-cubes fa-2x me-3" style="color: #ff6219;"></i>
+                                  <span class="h1 fw-bold mb-0">Just4Work</span>
+                              </div>
+
+                              <h5 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Register</h5>
+
+                              <div class="form-outline mb-4">
+                                  <b-form-input class="form-control rounded-10" type="Name" id="floatingInput" placeholder="Name"></b-form-input>
+                                  <label class="form-label" for="floatingInput">Name</label>
+                              </div>
+
+                              <div class="form-outline mb-4">
+                                  <b-form-input class="form-control rounded-10" type="Lastname" id="floatingInput" placeholder="Last Name"></b-form-input>
+                                  <label class="form-label" for="floatingInput">Cif</label>
+                              </div>
+
+                              <div class="form-outline mb-4">
+                                      <b-form-input
+                                        id="floatingInput"
+                                        class="form-control rounded-10"
+                                        placeholder="email@sm.com"
+                                        type="E-mail" 
+                                        v-model="email"
+                                      ></b-form-input>
+                                  <label class="form-label rounded-3" for="floatingInput">Email address</label>
+                              </div>
+
+                              <div class="form-outline mb-4">
+                                  <b-form-input v-model="password" type="Mobile" class="form-control rounded-3" id="floatingInput" placeholder="Movile"></b-form-input>
+                                  <label class="form-label" for="floatingInput">Movile</label>
+                              </div>
+
+
+                              <div class="pt-1 mb-4">
+                                      <b-button class="log btn btn-lg rounded-3 btn-md botonDeSubmit " type="submit" @click="submit">Login</b-button>
+                              </div>
+
+                              <a class="small text-muted" href="#!">Forgot password?</a>
+                              <a href="#!" class="small text-muted">Terms of use.</a>
+                              <a href="#!" class="small text-muted">Privacy policy</a>
+                              </b-form>
+
+                          </div>
+                          </div>
+                      </div>
+                      </div>
+                  </div>
+                  </div>
+              </div>
+      </section>
+  </div>
+  <!-- <div>
     <main>
       <div class="modal modal-signin position-static d-block py-5" tabindex="-1" role="dialog" id="modalSignin">
          <div class="modal-dialog" role="document">
@@ -15,9 +84,9 @@
                         <label for="floatingInput">Name</label>
                      </div>
                      <div class="form-floating mb-3">
-                        <input type="Lastname" class="form-control rounded-3" id="floatingInput"
+                           <input type="Lastname" class="form-control rounded-3" id="floatingInput"
                            placeholder="Last Name">
-                        <label for="floatingInput">Cif</label>
+                           <label for="floatingInput">Cif</label>
                      </div>
                      <div class="form-floating mb-3">
                         <input type="E-mail" class="form-control rounded-3" id="floatingInput"
@@ -40,7 +109,7 @@
    </main>
 
 
-  </div>
+  </div> -->
 </template>
 
 <script>
